adf3fa77dfcb1fdf494471cad2eb6ce31ada97e3
Jeni Dang <jenipdang@yahoo.com>
2022-03-18 16:46:47 -0700

    add user roles git add .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_231540) do
+ActiveRecord::Schema.define(version: 2022_03_18_232504) do
 
   create_table "suggestions", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_03_11_231540) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
