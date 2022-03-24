90647f4d277899533ef1a3e215c10e500346c331
Jeni Dang <jenipdang@yahoo.com>
2022-03-24 03:50:52 -0700

    clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,14 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    # set :views, 'app/views'
+
     set :default_content_type, :json
     enable :sessions
-    # use Rack::Session::Cookie, 
-    #   :key => 'rack.session',
-    #   :path => '/'
-      # :secret => ENV["SESSION_SECRET"]
-    set :session_secret, ENV["SESSION_SECRET"] #created an env variable and hide the keys inside
+    set :session_secret, ENV["SESSION_SECRET"] 
   end
 
   error ActiveRecord::RecordNotFound do
-    # 'Sorry there was a nasty error - ' + env['sinatra.error'].message
+    
     {errors: "Record not found with id #{params['id']}"}.to_json
   end
 
